1f5d7547fcf6599c336c9333ca23096f151c1904
arvinthrajasekar <rajaarvinth11@gmail.com>
2022-03-23 15:50:24 +0530

    5

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
 
 namespace StarChart.Controllers
@@ -13,6 +14,32 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
+        [HttpGet("{id:int}" ,Name ="GetById")]
+        public IActionResult GetById(int id)
+        {
+            var selectedId = _context.CelestialObjects.FirstOrDefault(p => p.Id == id);
+            if(selectedId == null)
+            {
+                return NotFound();
+            }
+            selectedId.Satellites.ForEach(s => s.Id = id);
+            return Ok(selectedId);
+       
+        }
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var selectedName = _context.CelestialObjects.FirstOrDefault(p => p.Name == name);
+            if(selectedName == null)
+            {
+                return NotFound();
+            }
+            selectedName.Satellites.ForEach(s => s.Name = name);
+            return Ok(selectedName);
+        }
+        
+       
+
 
     }
 }

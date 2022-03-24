dc91639a909393424a4265c396dd0a6472e5ba8e
arvinthrajasekar <rajaarvinth11@gmail.com>
2022-03-24 19:02:28 +0530

    5-6

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -22,7 +22,7 @@ namespace StarChart.Controllers
             {
                 return NotFound();
             }
-            selectedId.Satellites = _context.CelestialObjects.Select(s=> s.OrbitedObjectId );
+           // selectedId.Satellites = _context.CelestialObjects.Select(s=> s.OrbitedObjectId);
                 return Ok(selectedId);
        
         }
